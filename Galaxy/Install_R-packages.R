e339f70a451f7cee1bc21ba1d19e1f707c492f10
Sebastiaan de Vriend <sebastiaandevriend@gmail.com>
2017-05-13 14:59:11 +0200

    Add files via upload

---
+++ Galaxy/Install_R-packages.R
@@ -0,0 +1,5 @@
+#!/usr/bin/Rscript
+# install script for R packages. Please run this in bash, or R.
+options(rgl.useNULL=TRUE) # This option is used to install the packages in a shell env like linux.
+install.packages('Morpho')
+install.packages("ggplot2")
